31ce326bcba65a09c5a8a89f9f6886f08dfe506e
Sergiu Chilat <chilatsergiu@gmail.com>
2020-11-05 20:34:17 +0200

    JS. Instructiuni de decizie

---
+++ js/2. Instructiuni de decizie/app.js
@@ -0,0 +1,100 @@
+console.log("Azi e luni");
+console.log("Eu merg la lucru");
+console.log("Azi e marti");
+console.log("Eu merg la lucru");
+console.log("Azi e miercuri");
+console.log("Eu merg la lucru");
+console.log("Azi e joi");
+console.log("Eu merg la lucru");
+console.log("Azi e vineri");
+console.log("Eu merg la lucru");
+console.log("Azi e simbata");
+console.log("Eu NU merg la lucru");
+console.log("Azi e duminica");
+console.log("Eu NU merg la lucru");
+
+console.clear();
+
+let n = -1;
+if(n >-1 && n < 10){
+    console.log("Este cifra")
+} else{
+    console.log("Nu este cifra")
+}
+
+console.clear();
+
+let cifra = 11;
+
+if(cifra === 0){
+    console.log("Zero");
+}else 
+if(cifra === 1){
+    console.log("Unu");
+}else 
+if(cifra === 2){
+    console.log("Doinu");
+}else 
+if(cifra === 3){
+    console.log("Trei");
+}else 
+if(cifra === 4){
+    console.log("Patru");
+}else
+if(cifra === 5){
+    console.log("Cinci");
+}else 
+if(cifra === 6){
+    console.log("Sase");
+}else 
+if(cifra === 7){
+    console.log("Sapte");
+}else 
+if(cifra === 8){
+    console.log("Opt");
+}else 
+if(cifra === 9){
+    console.log("Noua");
+}else{
+    console.log("Nu este cifra");
+}
+
+console.clear();
+
+cifra = 11;
+switch(cifra){
+    case 0:
+        console.log("Zero");
+        break;
+    case 1:
+        console.log("Unu");
+        break;
+    case 2:
+        console.log("Doi");
+        break;
+    case 3: 
+        console.log("Trei");
+        break;
+    case 4:
+        console.log("Patru");
+        break;
+    case 5:
+        console.log("Cinci");
+        break;
+    case 6:
+        console.log("Sase");
+        break;
+    case 7:
+        console.log("Sapte");
+        break;
+    case 8:
+        console.log("Opt");
+        break;
+    case 9:
+        console.log("Noua");
+        break;
+    default:
+        console.log("Nu este cifra");
+        break;
+}
+
