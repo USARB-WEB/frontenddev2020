54334ccc914444a03a738cbe4834679ebf902f15
Sergiu Chilat <chilatsergiu@gmail.com>
2020-11-05 20:10:32 +0200

    JS. Sintaxa. Variabile

---
+++ js/1. Sintaxa/app.js
@@ -0,0 +1,51 @@
+console.log("Hello world!");
+
+let a = 4;
+let b = "Mesaj";
+let c;
+let d = false;
+let e = null;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+console.clear();
+
+
+a = 10;
+b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+console.clear();
+
+const x = 3;
+const y = 4;
+
+console.log(x + y);
+console.log(x - y);
+
+console.clear();
+
+const pi = 3.14;
+let r = 1;
+let S;
+
+S = pi * (r**2);
+console.log(S);
+
+
+console.clear();
+
+let s1 = "Hello";
+let s2 = " world!";
+
+let result = s1 + s2;
+console.log(result);
