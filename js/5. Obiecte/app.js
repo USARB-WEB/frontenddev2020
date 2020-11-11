70fd181a2c05ded463fe363c8e56f27b218b417d
Sergiu Chilat <chilatsergiu@gmail.com>
2020-11-11 20:25:01 +0200

    JS. Obiecte

---
+++ js/5. Obiecte/app.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "Ion Creanga",
+    age: 123,
+    phones: ["112", "223"],
+    address: {
+        street: "Stefan cel Mare",
+        house_number: 2,
+        apartment: 5
+    }
+};
+
+console.log(person);
+console.log(person.name);
+console.log(person.address.street);
