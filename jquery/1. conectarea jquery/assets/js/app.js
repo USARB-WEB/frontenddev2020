256a29596215f7860e4e50da724658f5e6d7ad45
Sergiu Chilat <chilatsergiu@gmail.com>
2020-11-25 19:48:44 +0200

    JQuery. Conectare

---
+++ jquery/1. conectarea jquery/assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    $("body").css({
+        backgroundColor: "blue"
+    });
+
+    $(".element").css({
+        backgroundColor: "red",
+        height: "50px"
+    });
+});
